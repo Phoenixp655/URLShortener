054f28ace92dafbbb9040dd364df6ec87f615258
hoanganhle <phoenixp655@gmail.com>
2023-02-21 19:22:00 +0700

    add urlencoded for post request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
 app.use('/public',express.static(__dirname + '/public'))
 
 
